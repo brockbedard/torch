adf609648d47d061f238120e477e8085899abab8
Brock Bedard <brock.j.bedard@gmail.com>
2026-03-24 20:06:36 -0600

    save progress before animation system build

---
+++ mockups/preview/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import TorchCardMockupFinal from '../torch-card-mockup-final.jsx';
+createRoot(document.getElementById('root')).render(<TorchCardMockupFinal />);
